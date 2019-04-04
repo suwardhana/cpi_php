b1439a4f4043e3aef9c4d42bc8351afeefd1e180
Suwardhana <spensajambi@gmail.com>
2019-04-04 09:47:16 +0700

    tampil nilai

---
+++ tampil_nilai.php
@@ -0,0 +1,42 @@
+<?php include_once("template/header.php") ?>
+
+<?php 
+
+include('class/mysql_crud.php');
+$db = new Database();
+$db->connect();
+$db->sql('select nama, nama_kriteria, nilai from nilai join calon_peminjam on nilai.id_calon=calon_peminjam.id_calon join kriteria on nilai.id_kriteria=kriteria.id_kriteria'); // Table name, Column Names, JOIN, WHERE conditions, ORDER BY conditions
+$res = $db->getResult();
+?>
+
+<div class="row">
+  <div class="card-panel">
+    <h5>Data Calon Peminjam</h5>
+    <table class="bordered">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Nama Calon Peminjam</th>
+          <th>Kategori</th>
+          <th>Nilai</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php $i=1;
+            foreach ($res as $a) { ?>
+        <tr>
+          <td><?= $i ?></td>
+          <td><?= $a['nama'] ?></td>
+          <td><?= $a['nama_kriteria'] ?></td>
+          <td><?= number_format($a['nilai']) ?></td>
+        </tr>
+        <?php
+                $i++;
+            }
+            ?>
+      </tbody>
+    </table>
+  </div>
+
+</div>
+<?php include_once("template/footer.php") ?>
