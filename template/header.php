dec8f5b3a42cc6dafbb511860f40714c55c8578c
Suwardhana <spensajambi@gmail.com>
2019-04-04 21:29:32 +0700

    menu

--- template/header.php
+++ template/header.php
@@ -2,23 +2,27 @@
 <html lang="en">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
-    <title>DSS - CPI</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+  <title>DSS - CPI</title>
 
-    <!-- CSS  -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
 </head>
 
 <body>
-    <nav>
-        <div class="nav-wrapper">
-            <a href="#" class="brand-logo">LOGO</a>
-            <ul class="right hide-on-med-and-down">
-                <li><a href="#">Navbar Link</a></li>
-            </ul>
-        </div>
-    </nav>
-    <div class="section no-pad-bot" id="index-banner">
-        <div class="container">
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">DSS CPI</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="calon_peminjam.php">Calon Peminjam</a></li>
+        <li><a href="kriteria.php">Kriteria</a></li>
+        <li><a href="tampil_nilai.php">Nilai</a></li>
+        <li><a href="cpi.php">CPI</a></li>
+
+      </ul>
+    </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
